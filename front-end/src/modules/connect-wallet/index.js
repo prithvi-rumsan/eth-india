caa85069dc4349db9b86bcf49ec57bf33a531563
sanjay-del <sanjaypantha.np@gmail.com>
2023-12-09 23:10:40 +0545

    Wallet auth addition

--- front-end/src/modules/connect-wallet/index.js
+++ front-end/src/modules/connect-wallet/index.js
@@ -3,13 +3,47 @@ import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import "./connect-wallet.css";
 import Card from "../../components/card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { injected } from "../../utils/wallet";
+import { useWeb3React } from "@web3-react/core";
 
 function ConnectWallet() {
-  const handleConnectWallet = () => {
-    console.log("HANDLE CONNECT WALLET");
-  };
+  const { activate,deactivate } = useWeb3React();
+
+
+  const connectMetaMask = async () => {
+    console.log("connectMetaMask")
+    try {
+      if (typeof window.ethereum === "undefined") {
+        console.log("MetaMask is not installed!");
+        return false;
+      }
+      await activate(injected);
+      console.log("activate")
+      localStorage.setItem("wallet-auth", "MetaMask");
+      window.location.href = "/dashboard";
+      // await contractCall()
+      return true;
+    } catch (e) {
+      console.log(e);
+      localStorage.removeItem("wallet-auth");
+    }
+  }
+
+  const disconnectWallet = async () => {  
+    try{
+      deactivate();
+      localStorage.removeItem("wallet-auth");
+
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+
+
+
+
   return (
     <Container className="full-center">
       <Card>
@@ -20,7 +54,8 @@ function ConnectWallet() {
           </div>
         </div>
         <br />
-        <Button onClick={handleConnectWallet}>Connect</Button>
+        <Button onClick ={connectMetaMask}>  Connect</Button>
+        <Button onClick ={disconnectWallet}>Disconnect</Button>
       </Card>
     </Container>
   );
