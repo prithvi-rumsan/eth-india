8ab99ab2fb88384c2be49115af28d4cf47b4e393
prithvi-rumsan <maharjanprithvi@gmail.com>
2023-12-09 23:44:34 +0545

    UI changes

--- front-end/src/modules/connect-wallet/index.js
+++ front-end/src/modules/connect-wallet/index.js
@@ -9,16 +9,15 @@ import "./connect-wallet.css";
 function ConnectWallet() {
   const { activate, deactivate } = useWeb3React();
 
-
   const connectMetaMask = async () => {
-    console.log("connectMetaMask")
+    console.log("connectMetaMask");
     try {
       if (typeof window.ethereum === "undefined") {
         console.log("MetaMask is not installed!");
         return false;
       }
       await activate(injected);
-      console.log("activate")
+      console.log("activate");
       localStorage.setItem("wallet-auth", "MetaMask");
       window.location.href = "/dashboard";
       // await contractCall()
@@ -27,38 +26,36 @@ function ConnectWallet() {
       console.log(e);
       localStorage.removeItem("wallet-auth");
     }
-  }
+  };
 
   const disconnectWallet = async () => {
     try {
       deactivate();
       localStorage.removeItem("wallet-auth");
-
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
     }
-  }
-
-
-
-
+  };
 
   return (
     <Container className="full-center">
       <div className="card-container">
         <Card>
           <div className="title-container">
-            Connect To Metamask
+            Connect Metamask Wallet
             <div className="image-container">
               <Image src="/metamask.png" fluid />
             </div>
           </div>
           <br />
           <br />
-          <Button onClick={connectMetaMask}>  Connect</Button>
-          <Button onClick={disconnectWallet}>Disconnect</Button>
-        </Card >
+          <Button onClick={connectMetaMask} style={{ marginBottom: "10px" }}>
+            Connect
+          </Button>
+          <Button onClick={disconnectWallet} variant="danger">
+            Disconnect
+          </Button>
+        </Card>
       </div>
     </Container>
   );
