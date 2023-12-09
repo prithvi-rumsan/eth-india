d081e3b30c21100ada752e83479aba40a74e5459
prithvi-rumsan <maharjanprithvi@gmail.com>
2023-12-09 15:21:57 +0545

    Added blockchain constants

---
+++ front-end/src/constants/blockchainConstants.js
@@ -0,0 +1,30 @@
+module.exports = {
+  DEPLOYEMNT_NETWORK: "testnet",
+  CONTRACT_ADDRESS: {
+    dao: "0xc84aB98e3ed6BF6F3a2673C4CCCF9473f5a402B3",
+  },
+  BLOCKCHAIN_NETWORK: {
+    SCROLL_TESTNET: "97",
+  },
+  CHAIN_ID: {
+    SCROLL: 534351,
+
+  },
+  CHAIN_RPC_URLS: {
+    SCROLL_RPC: "https://sepolia-rpc.scroll.io",
+  },
+  EXPLORERS: {
+    SCROLL_EXPLORER: "https://sepolia.scrollscan.dev",
+  },
+  SYMBOLS: {
+    SCROLL_SYMBOL: "SepoliaETH",
+  },
+
+  NATIVE_CURRENCY: {
+    SCROLL_CUR: "SepoliaETH",
+  },
+  AVAILABLE_NETWORKS: [
+    { label: "Sroll testnet", value: 1 },
+  ],
+
+};
---
+++ front-end/src/constants/index.js
@@ -0,0 +1,4 @@
+const PUSH_KEY = process.env.REACT_APP_PUSH_KEY;
+module.exports = {
+  PUSH_KEY,
+};
