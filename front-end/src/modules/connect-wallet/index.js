9a04a55ff2a3e70520d948c633b446017a267ae7
prithvi-rumsan <maharjanprithvi@gmail.com>
2023-12-09 19:36:59 +0545

    UI updates

--- front-end/src/modules/connect-wallet/index.js
+++ front-end/src/modules/connect-wallet/index.js
@@ -1,18 +1,27 @@
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import "./connect-wallet.css";
+import Card from "../../components/card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 
 function ConnectWallet() {
+  const handleConnectWallet = () => {
+    console.log("HANDLE CONNECT WALLET");
+  };
   return (
-    <Container>
-      <Row>
-        <Col></Col>
-        <Col>
-          <Button>Connect Wallet</Button>
-        </Col>
-        <Col></Col>
-      </Row>
+    <Container className="full-center">
+      <Card>
+        <div className="title-container">
+          Connect To Metamask
+          <div className="image-container">
+            <Image src="/metamask.png" fluid />
+          </div>
+        </div>
+        <br />
+        <Button onClick={handleConnectWallet}>Connect</Button>
+      </Card>
     </Container>
   );
 }
